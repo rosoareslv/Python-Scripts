110bb55ad19e5cc006098e64d5aa82e4e13bffc3
Rodrigo Soares <rodrigosoaresilva@gmail.com>
2025-06-01 20:06:36 -0300

    adding rock paper scissors game

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,20 +1,21 @@
 import random
 
-class Player():
-    
+
+class Player:
+
     def __init__(self):
         self.moves = []
         self.score = 0
-    
+
     def sum_round_victory(self):
         self.score += 1
-    
+
     def store_move(self, option):
         self.moves.append(option)
-        
+
 
 class HumanPlayer(Player):
-    
+
     def choose_move(self):
         while True:
             option = input("Rock, paper or scissors [r/p/s]? ")
@@ -23,26 +24,25 @@ class HumanPlayer(Player):
             else:
                 self.store_move(option)
                 return option
-            
+
+
 class ComputerPlayer(Player):
-    
-    options = ["r","p","s"]
-    
+
+    options = ["r", "p", "s"]
+
     def choose_move(self):
         option = random.choice(ComputerPlayer.options)
         self.store_move(option)
         return option
-        
 
-class Game():
-    
-    
+
+class Game:
+
     def __init__(self, human, computer, rounds):
         self.human = human
         self.computer = computer
         self.rounds = rounds
-    
-            
+
     def play_game(self):
         counter = 0
         while counter < self.rounds:
@@ -53,7 +53,7 @@ class Game():
                 self.human.sum_round_victory()
             else:
                 self.computer.sum_round_victory()
-               
+
             counter += 1
             self.__summarize()
         result = self.__decide_winner()
@@ -61,50 +61,54 @@ class Game():
             print(f"Your moves: {'|'.join(self.human.moves)}")
         else:
             print(f"Computer moves: {'|'.join(self.computer.moves)}")
-            
-        
-                
+
     def __get_round_victory(self, human_option, computer_option):
         print(f"You: {human_option} | Computer: {computer_option}")
         if human_option == computer_option:
             print("This round is a tie")
-        elif ((human_option == 'r' and computer_option == "s") 
-            or (human_option == 'p' and computer_option == "r")
-            or (human_option == 's' and computer_option == "p")):
-                print("You won this round!")
-                return 1
+        elif (
+            (human_option == "r" and computer_option == "s")
+            or (human_option == "p" and computer_option == "r")
+            or (human_option == "s" and computer_option == "p")
+        ):
+            print("You won this round!")
+            return 1
         else:
             print("You lost this round!")
             return 0
-            
+
     def __summarize(self):
-        print(f'[Game Summary] Your points: {self.human.score} | Computer Points {self.computer.score}')
-    
+        print(
+            f"[Game Summary] Your points: {self.human.score} | Computer Points {self.computer.score}"
+        )
+
     def __decide_winner(self):
         if self.human.score == self.computer.score:
             print("The game is a tie")
         elif self.human.score > self.computer.score:
-            print('You won')
+            print("You won")
             return 1
         else:
             print("You lost")
             return 0
-        
+
+
 def start_menu():
-        print("Rock Paper Scissors Game")
-        while True:
-            try:
-                return int(input("How many rounds do you want to play? "))
-            except ValueError:
-                print("Enter a valid number")
-                
+    print("Rock Paper Scissors Game")
+    while True:
+        try:
+            return int(input("How many rounds do you want to play? "))
+        except ValueError:
+            print("Enter a valid number")
+
+
 def main():
-   rounds = start_menu()
-   human = HumanPlayer()
-   machine = ComputerPlayer()
-   game = Game(human, machine, rounds)
-   game.play_game()
-   
+    rounds = start_menu()
+    human = HumanPlayer()
+    machine = ComputerPlayer()
+    game = Game(human, machine, rounds)
+    game.play_game()
+
 
 if __name__ == "__main__":
-   main()
+    main()
